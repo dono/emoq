23a655e2d8b69cd5594d32512fef169a558c1d05
dono <xononeet@gmail.com>
2021-04-24 23:59:46 +0900

    Add prototype

---
+++ empath_trial.go
@@ -0,0 +1,16 @@
+package main
+
+
+
+func main() {
+
+  endPoint := "https://api.webempath.net/v2/analyzeWav"
+
+  apiKey = ""
+  // payload = {"apikey": apiKey}
+
+  wav := "/path/to/hoge.wav"
+  // file := openWav()
+  // res := request
+
+}
